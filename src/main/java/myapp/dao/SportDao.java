0066e4067ad5231d9b0eca1829eed7588ff188af
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2018-10-18 20:47:38 +0200

    correction des classes

--- src/main/java/myapp/dao/SportDao.java
+++ src/main/java/myapp/dao/SportDao.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import myapp.javaObjects.Personne;
 import myapp.javaObjects.Sport;
 
 @Transactional
@@ -15,4 +16,7 @@ public interface SportDao extends JpaRepository<Sport, Long>{
 	
 	@Query("select s from Sport s where s.name = 'Mer'")
 	public List<Sport> giveSportNautique();
+	
+	@Query("select s from Sport s Where s.name = ?1")
+	public Sport findByName(String name);
 }
