ebe60d94e34e68974febea035bb1897f42a9a01f
mjj94 <m.jammal94@gmail.com>
2018-10-21 18:02:14 +0200

    enlever la liste de personne

--- taa-back/src/main/java/myapp/javaObjects/Lieu.java
+++ taa-back/src/main/java/myapp/javaObjects/Lieu.java
@@ -1,14 +1,9 @@
 package myapp.javaObjects;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity
 public class Lieu implements Serializable{
@@ -20,8 +15,6 @@ public class Lieu implements Serializable{
 	private Long id;
 
 	    private String name;
-
-	    private List<Personne> personnes = new ArrayList<Personne>();
 
 	    public Lieu() {
 	        super();
@@ -47,15 +40,5 @@ public class Lieu implements Serializable{
 
 	    public void setName(String name) {
 	        this.name = name;
-	    }
-
-	    @ManyToMany(mappedBy = "lieu", cascade = CascadeType.PERSIST)
-	    public List<Personne> getPersonnes() {
-	        return personnes;
-	    }
-	    
-
-	    public void setPersonnes(List<Personne> personnes) {
-	        this.personnes = personnes;
 	    }
 }
--- taa-back/src/main/java/myapp/javaObjects/Sport.java
+++ taa-back/src/main/java/myapp/javaObjects/Sport.java
@@ -1,61 +1,45 @@
 package myapp.javaObjects;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 @Entity
-public class Sport implements Serializable{
-	 /**
-	 * 
-	 */
+public class Sport implements Serializable {
+	/**
+	* 
+	*/
 	private static final long serialVersionUID = -2959708523025785328L;
 
 	private Long id;
 
-	    private String name;
+	private String name;
 
-	    private List<Personne> personnes = new ArrayList<Personne>();
+	public Sport() {
+		super();
+	}
 
-	    public Sport() {
-	        super();
-	    }
+	public Sport(String name) {
+		this.name = name;
+	}
 
-	    public Sport(String name) {
-	        this.name = name;
-	    }
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
 
-	    @Id
-	    @GeneratedValue
-	    public Long getId() {
-	        return id;
-	    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
+	public String getName() {
+		return name;
+	}
 
-	    public String getName() {
-	        return name;
-	    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-
-	    @ManyToMany(mappedBy = "sport", cascade = CascadeType.PERSIST)
-	    public List<Personne> getPersonnes() {
-	        return personnes;
-	    }
-	    
-
-	    public void setPersonnes(List<Personne> personnes) {
-	        this.personnes = personnes;
-	    }
 }
