682a143a90393c046dd3b24ee246a993ec8ef4bf
mjj94 <m.jammal94@gmail.com>
2018-11-11 21:29:41 +0100

    init de la bdd

---
+++ taa-back/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+TRUNCATE place;
+TRUNCATE sport;
+
+LOAD DATA LOCAL INFILE '/home/mj/taa-workspace/taa/taa-back/src/main/resources/cities.csv'
+INTO TABLE place
+FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(id, name);
+
+LOAD DATA LOCAL INFILE '/home/mj/taa-workspace/taa/taa-back/src/main/resources/sports.csv'
+INTO TABLE sport
+FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(id, name, covered);
